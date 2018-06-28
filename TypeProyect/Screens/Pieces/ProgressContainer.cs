f979238e13c1b5d3ba36529148026bdcf38ccb2e
ColdVolcano <mineflamecraft@hotmail.com>
2018-06-28 02:18:44 -0500

    Allow for seek, position viewing, current time viewing and total time viewing on ProgressContainer

--- TypeProyect/Screens/Pieces/ProgressContainer.cs
+++ TypeProyect/Screens/Pieces/ProgressContainer.cs
@@ -9,6 +9,10 @@ using OpenTK;
 using Newtonsoft.Json;
 using System.IO;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics.Transforms;
+using System;
+using osu.Framework.Input;
 
 namespace TypeProyect.Screens.Pieces
 {
@@ -20,50 +24,83 @@ namespace TypeProyect.Screens.Pieces
         private EquilateralTriangle triangle;
         private List<string> metadataList = new List<string>();
         private int index = -1;
+        private Container barContainer;
         private Container progress;
         private SpriteText timeText;
+        private SpriteText totalTimeText;
+        private SpriteText hoverTimeText;
+        private Bindable<bool> barsHovered = new Bindable<bool>();
 
         public ProgressContainer()
         {
-            AutoSizeAxes = Axes.Y;
+            Height = bar_height;
             Origin = Anchor.BottomLeft;
             Children = new Drawable[]
             {
                 timeText = new SpriteText
                 {
+                    AlwaysPresent = true,
                     Anchor = Anchor.TopLeft,
                     Origin = Anchor.TopLeft,
                     Text = "0:00",
                     Font = "Exo2.0-Bold",
-                    TextSize = 35,
+                    TextSize = 45,
+                    Position = new Vector2(0, -45),
                 },
-                triangle = new EquilateralTriangle
+                totalTimeText = new SpriteText
                 {
-                    Rotation = 180,
-                    EdgeSmoothness = new Vector2(1),
+                    AlwaysPresent = true,
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Text = "x:xx",
+                    Font = "Exo2.0-Bold",
+                    TextSize = 45,
+                    Position = new Vector2(0, -45),
+                },
+                hoverTimeText = new SpriteText
+                {
                     RelativePositionAxes = Axes.X,
-                    Size = new Vector2(13),
-                    Anchor = Anchor.BottomLeft,
-                    Origin = Anchor.TopCentre,
-                    Position = new Vector2(0, -41.5f),
-                },
-                new Box
-                {
-                    RelativeSizeAxes = Axes.X,
-                    Colour = new Color4(25, 25, 25, 255),
-                    Height = bar_height,
-                    Anchor = Anchor.BottomLeft,
-                    Origin = Anchor.BottomLeft,
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.BottomCentre,
+                    Text = "0:00",
+                    Font = "Exo2.0-Bold",
+                    TextSize = 35,
+                    Shadow = true,
+                    Position = new Vector2(0, -15)
                 },
                 new Container
                 {
+                    Origin = Anchor.BottomLeft,
                     RelativeSizeAxes = Axes.X,
-                    Height = 40,
+                    Height = 100,
+                    Masking = true,
+                    Child = triangle = new EquilateralTriangle
+                    {
+                        Rotation = 180,
+                        EdgeSmoothness = new Vector2(1),
+                        RelativePositionAxes = Axes.X,
+                        Size = new Vector2(13),
+                        Anchor = Anchor.BottomLeft,
+                        Origin = Anchor.TopCentre,
+                        Position = new Vector2(0, 13),
+                    },
+                },
+                barContainer = new Container
+                {
+                    RelativeSizeAxes = Axes.X,
+                    Height = bar_height,
                     Masking = true,
                     Anchor = Anchor.BottomLeft,
                     Origin = Anchor.BottomLeft,
                     Children = new Drawable[]
                     {
+                        new Box
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Colour = new Color4(25, 25, 25, 255),
+                            Anchor = Anchor.BottomLeft,
+                            Origin = Anchor.BottomLeft,
+                        },
                         progress = new Container
                         {
                             RelativeSizeAxes = Axes.Both,
@@ -81,6 +118,24 @@ namespace TypeProyect.Screens.Pieces
                     }
                 }
             };
+
+        }
+
+        private void trianglePopUp(bool value)
+        {
+            if (proyect.Metadata.Value?.Track?.IsLoaded ?? false)
+            {
+                triangle.MoveToY(value ? -1 : 13, 200, Easing.OutCubic);
+                timeText.ResizeTextHeightTo(value ? 45 - 14f : 45, 200, Easing.OutCubic);
+                if (!value)
+                    timeText.FadeIn(200, Easing.OutCubic);
+                xyz = false;
+                totalTimeText.ResizeTextHeightTo(value ? 45 - 14f : 45, 200, Easing.OutCubic);
+                if (!value)
+                    totalTimeText.FadeIn(200, Easing.OutCubic);
+                zyx = false;
+                hoverTimeText.FadeTo(value ? 1 : 0, 200, Easing.OutCubic);
+            }
         }
 
         [BackgroundDependencyLoader]
@@ -96,6 +151,9 @@ namespace TypeProyect.Screens.Pieces
             using (StreamReader r = new StreamReader(storage.GetStream("list.json")))
                 metadataList = JsonConvert.DeserializeObject<List<string>>(r.ReadToEnd());
             playNext();
+            proyect.Cache(this);
+            barsHovered.ValueChanged += trianglePopUp;
+            barsHovered.TriggerChange();
         }
 
         private void playNext()
@@ -103,32 +161,114 @@ namespace TypeProyect.Screens.Pieces
             if (index == metadataList.Count - 1)
                 index = -1;
 
-            using (StreamReader r = new StreamReader(storage.GetStream(metadataList[++index])))
+            play(true);
+        }
+
+        private void playBefore()
+        {
+            if (index == 0)
+                index = metadataList.Count + 1;
+
+            play(false);
+        }
+
+        private void play(bool next)
+        {
+            using (StreamReader r = new StreamReader(storage.GetStream(metadataList[next ? ++index : --index])))
             {
                 AudioMetadata meta = JsonConvert.DeserializeObject<AudioMetadata>(r.ReadToEnd());
                 meta.InitializeComponents(storage);
                 proyect.Metadata.Value = meta;
-                progress.ResizeWidthTo(0, 750, Easing.OutExpo);
                 proyect.Audio.Track.AddItemToList(meta.Track);
                 meta.Track.Restart();
             }
+            totalTimeText.Text = formatTime(TimeSpan.FromMilliseconds(proyect.Metadata.Value?.Track?.Length ?? 0));
         }
+
+        private string formatTime(TimeSpan t) => $"{Math.Floor(t.Duration().TotalMinutes)}:{t.Duration().Seconds:D2}";
 
         protected override void Update()
         {
             base.Update();
-            
+            barsHovered.Value = barContainer.IsHovered;
+
             var track = proyect.Metadata.Value?.Track;
 
             if (track?.IsLoaded ?? false)
             {
                 float pp = (float)(track.CurrentTime / track.Length);
                 progress.ResizeWidthTo(pp);
-                triangle.MoveToX(pp);
+                //triangle.MoveToX(pp);
+                timeText.Text = formatTime(TimeSpan.FromMilliseconds(track.CurrentTime));
 
                 if (track.HasCompleted && !track.Looping)
                     playNext();
             }
         }
+
+        private bool xyz = false;
+        private bool zyx = false;
+
+        protected override bool OnMouseMove(InputState state)
+        {
+            if (barsHovered)
+            {
+                float f = ToLocalSpace(state.Mouse.Position).X / barContainer.DrawSize.X;
+                triangle.MoveToX(f);
+                hoverTimeText.MoveToX(f);
+                hoverTimeText.Text = formatTime(TimeSpan.FromMilliseconds((proyect.Metadata.Value?.Track?.Length ?? 0) * f));
+                if (hoverTimeText.BoundingBox.IntersectsWith(timeText.BoundingBox))
+                {
+                    if (!xyz)
+                    {
+                        timeText.FadeOut(200, Easing.OutCubic);
+                        xyz = true;
+                    }
+                }
+                else if (xyz)
+                {
+                    timeText.FadeIn(200, Easing.OutCubic);
+                    xyz = false;
+                }
+                if (hoverTimeText.BoundingBox.IntersectsWith(totalTimeText.BoundingBox))
+                {
+                    if (!zyx)
+                    {
+                        totalTimeText.FadeOut(200, Easing.OutCubic);
+                        zyx = true;
+                    }
+                }
+                else if (zyx)
+                {
+                    totalTimeText.FadeIn(200, Easing.OutCubic);
+                    zyx = false;
+                }
+            }
+            return base.OnMouseMove(state);
+        }
+
+        protected override bool OnDragStart(InputState state)
+        {
+            return true;
+        }
+
+        protected override bool OnClick(InputState state)
+        {
+            var t = proyect.Metadata.Value?.Track;
+            if (barsHovered && (t?.IsLoaded ?? false))
+            {
+                float f = ToLocalSpace(state.Mouse.Position).X / barContainer.DrawSize.X;
+                progress.ResizeWidthTo(f, 500, Easing.OutExpo);
+                progress.Delay(500);
+                t.Seek(f * t.Length);
+            }
+            return base.OnClick(state);
+        }
+    }
+
+    public static class SpriteTextExtensions
+    {
+        public static TransformSequence<T> ResizeTextHeightTo<T>(this T drawable, float newHeight, double duration = 0, Easing easing = Easing.None) where T : SpriteText =>
+           drawable.TransformTo(nameof(drawable.TextSize), newHeight, duration, easing);
     }
 }
