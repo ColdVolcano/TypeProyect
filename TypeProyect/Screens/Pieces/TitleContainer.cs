40df3ba420be71c5172adcbd2895bd71a43af49d
ColdVolcano <mineflamecraft@hotmail.com>
2018-06-28 02:18:04 -0500

    Kindly try to start TitleContainer

--- TypeProyect/Screens/Pieces/TitleContainer.cs
+++ TypeProyect/Screens/Pieces/TitleContainer.cs
@@ -1,11 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using osu.Framework.Configuration;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Localisation;
 
 namespace TypeProyect.Screens.Pieces
 {
     public class TitleContainer : Container
     {
+        protected UnicodeBindableString Text = new UnicodeBindableString("ダタがない", "No data");
+
+        private SpriteText mainText;
+        private SpriteText subText;
+
+        public float TextSize
+        { 
+            get => textSize;
+            set
+            {
+                mainText.TextSize = value;
+                subText.TextSize = value;
+                textSize = value;
+            }
+        }
+
+        private float textSize;
+
+        /// <summary>
+        /// The ammount of delay used when the value of <see cref="Text"/> changes.
+        /// </summary>
+        public double TransitionDelay = 0;
+
+        public TitleContainer(float textSize)
+        {
+        }
     }
 }
