916f0de084e6d78d79e12053e3fddc94bb71a577
ColdVolcano <mineflamecraft@hotmail.com>
2018-06-29 20:18:09 -0500

    Implement TitleContainer

--- TypeProyect/Screens/Loader.cs
+++ TypeProyect/Screens/Loader.cs
@@ -1,6 +1,8 @@
 ﻿using OpenTK;
+using OpenTK.Graphics;
 using osu.Framework.Allocation;
 using osu.Framework.Configuration;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shaders;
 using osu.Framework.Platform;
 using osu.Framework.Screens;
@@ -31,7 +33,7 @@ namespace TypeProyect.Screens
             loadTargets.Add(manager.Load(VertexShaderDescriptor.TEXTURE_2, FragmentShaderDescriptor.TEXTURE));
             loadTargets.Add(manager.Load(VertexShaderDescriptor.TEXTURE_3, FragmentShaderDescriptor.TEXTURE_ROUNDED));
             loadTargets.Add(manager.Load(VertexShaderDescriptor.TEXTURE_3, FragmentShaderDescriptor.TEXTURE));
-            config.GetBindable<WindowMode>(FrameworkSetting.WindowMode).Value = WindowMode.Fullscreen;
+            config.GetBindable<WindowMode>(FrameworkSetting.WindowMode).Value = WindowMode.Borderless;
             config.GetBindable<FrameSync>(FrameworkSetting.FrameSync).Value = FrameSync.Unlimited;
         }
 
@@ -45,13 +47,31 @@ namespace TypeProyect.Screens
                 Width = 1536,
             }, Add);
 
-            game.LoadComponentSingleFile(triangles = new Triangles() { Velocity = 2.5f }, Add);
+            game.LoadComponentSingleFile(triangles = new Triangles { Velocity = 2.5f }, Add);
             game.LoadComponentSingleFile(coverContainer = new CoverContainer()
             {
                 Position = new Vector2(192, 140),
-                Size = new Vector2(325),
+                Size = new Vector2(350),
             }, Add);
-            game.LoadComponentSingleFile(new Visualisation()
+            game.LoadComponentSingleFile(new TitleContainer(Anchor.BottomLeft)
+            {
+                Position = new Vector2(550, 140),
+                TextSize = 110,
+                Font = "Exo2.0-Bold",
+                PreferredText = MetadataTypes.TitleUnicode,
+                Width = 1176,
+            }, Add);
+            game.LoadComponentSingleFile(new TitleContainer(Anchor.TopLeft)
+            {
+                Position = new Vector2(550, 365),
+                TextSize = 60,
+                Font = "Exo2.0-Medium",
+                PreferredText = MetadataTypes.ArtistUnicode,
+                Width = 1176,
+                Colour = new Color4(200, 200, 200, 255),
+                TransitionDelay = 75,
+            }, Add);
+            game.LoadComponentSingleFile(new Visualisation
             {
                 Position = new Vector2(192, 614),
                 Size = new Vector2(1536, 460),
--- TypeProyect/Screens/Pieces/TitleContainer.cs
+++ TypeProyect/Screens/Pieces/TitleContainer.cs
@@ -1,37 +1,95 @@
 ﻿using osu.Framework.Configuration;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Localisation;
+using osu.Framework.Graphics;
+using OpenTK;
+using osu.Framework.Allocation;
 
 namespace TypeProyect.Screens.Pieces
 {
     public class TitleContainer : Container
     {
-        protected UnicodeBindableString Text = new UnicodeBindableString("ダタがない", "No data");
+        private Bindable<AudioMetadata> meta = new Bindable<AudioMetadata>();
 
-        private SpriteText mainText;
-        private SpriteText subText;
+        private TextFlowContainer mainText;
+        private Container mainContainer;
+
+        private Anchor textAnchor;
+
+        public MetadataTypes PreferredText = MetadataTypes.TitleUnicode;
 
         public float TextSize
-        { 
+        {
             get => textSize;
             set
             {
-                mainText.TextSize = value;
-                subText.TextSize = value;
+                Height = value * 2;
                 textSize = value;
             }
         }
 
-        private float textSize;
+        private float textSize = 75;
+
+        public string Font = "Exo2.0";
 
         /// <summary>
         /// The ammount of delay used when the value of <see cref="Text"/> changes.
         /// </summary>
         public double TransitionDelay = 0;
 
-        public TitleContainer(float textSize)
+        public TitleContainer(Anchor textAnchor)
         {
+            this.textAnchor = textAnchor;
+            Masking = true;
+            Children = new Drawable[]
+            {
+                mainContainer = new Container
+                {
+                    Masking = true,
+                    RelativePositionAxes = Axes.Y,
+                    RelativeSizeAxes = Axes.Both,
+                    Child = mainText = new TextFlowContainer
+                    {
+                        Anchor = textAnchor,
+                        Origin = textAnchor,
+                        TextAnchor = textAnchor,
+                        RelativeSizeAxes = Axes.X,
+                        AutoSizeAxes = Axes.Y,
+                    }
+                }
+            };
         }
+
+        [BackgroundDependencyLoader]
+        private void load(TypeProyect type)
+        {
+            meta.BindTo(type.Metadata);
+            meta.ValueChanged += transition;
+            meta.TriggerChange();
+        }
+
+        private void transition(AudioMetadata newMeta)
+        {
+            mainContainer.Delay(TransitionDelay)
+                .MoveToY((int)(textAnchor & Anchor.y0) == 1 ? -1 : 1, 212.5, Easing.InQuart)
+                .OnComplete(_ =>
+                {
+                    mainText.Clear();
+                    mainText.AddText((string)(newMeta.GetType().GetField(PreferredText.ToString()).GetValue(newMeta)) ?? string.Empty, s =>
+                    {
+                        s.TextSize = textSize;
+                        s.Font = Font;
+                    });
+                    mainContainer.MoveToY(0, 212.5, Easing.OutQuad);
+                });
+        }
+    }
+
+    public enum MetadataTypes
+    {
+        TitleUnicode,
+        Title,
+        Artist,
+        ArtistUnicode
     }
 }
